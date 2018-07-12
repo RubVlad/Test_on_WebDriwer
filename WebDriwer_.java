30027840d3288b3569881593b4e906ad9d462aec
RubVlad <40755490+RubVlad@users.noreply.github.com>
2018-07-12 16:06:34 +0300

    Update WebDriwer_.java

--- WebDriwer_.java
+++ WebDriwer_.java
@@ -1,33 +1,49 @@
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertEquals;
+
 
 public class WebDriwer_ {
 
    /* public static void main(String[] args) {
 
     }*/
+   private static WebDriver driver;
+
+
+    @BeforeClass
+    public static void setup() {
+        driver = new ChromeDriver();
+        driver.get("http://www.i.ua/");
+    }
 
 
 
     @Test
     public void SeleniumTest()
     {
-        WebDriver driver = new ChromeDriver();
-
         String Exp_Name="Vasya";
 
+        driver.findElement(By.cssSelector("[name=\"login\"]")).sendKeys("Vasyaaaa");
+        driver.findElement(By.cssSelector("[name=\"pass\"]")).sendKeys("VasyaPass123");
+        driver.findElement(By.cssSelector("[tabindex=\"5\"]")).click();
 
-        driver.get("http://www.i.ua/");
-        driver.findElement(By.CssSelector("[name=\"login\"]")).sendKeys("Vasyaaaa");
-        driver.findElement(By.CssSelector("[name=\"pass\"]")).sendKeys("VasyaPass123");
-        driver.findElement(By.CssSelector("[tabindex=\"5\"]")).Click();
+        String ResponseName=driver.findElement(By.cssSelector(".user_name")).getText();
 
-        String ResponseName=driver.findElement(By.CssSelector(".user_name")).innerText;
+        //assertEquals(Exp_Name,ResponseName);
+        Assert.assertEquals(Exp_Name,ResponseName);
 
-        assertEquals(Exp_Name,ResponseName);
+    }
+
+    @AfterClass
+    public static void tearDown() {
         driver.quit();
     }
 }
